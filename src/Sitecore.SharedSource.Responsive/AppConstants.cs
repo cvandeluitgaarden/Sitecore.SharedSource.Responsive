adc0e74248c632589b4bc0753fb2dc809663b7e3
Christian van de Luitgaarden <c.vandeluitgaarden@outlook.com>
2015-10-02 22:51:49 +0200

    Initial commit

---
+++ src/Sitecore.SharedSource.Responsive/AppConstants.cs
@@ -0,0 +1,22 @@
+﻿namespace Sitecore.SharedSource.Responsive
+{
+    using Data;
+    using System;
+
+    public class AppConstants
+    {
+        public static readonly string[] ResponsiveSectionNames = new string[]
+        {
+            "Blockgrid",
+            "Grid alignment",
+            "Grid collapse",
+            "Grid offset",
+            "Grid pull",
+            "Grid push",
+            "Grid size",
+            "Visibility"
+        };
+
+        public static readonly string ParameterTemplateName = "Parameters template";
+    }
+}
---
+++ src/Sitecore.SharedSource.Responsive/Extensions/SitecoreHelperExtensions.cs
@@ -0,0 +1,47 @@
+﻿namespace Sitecore.SharedSource.Responsive.Extensions
+{
+    using Mvc.Helpers;
+    using Data.Fields;
+    using System.Web;
+    using Mvc.Presentation;
+    using System.Linq;
+    using System;
+    using System.Collections.Generic;
+
+    public static class SitecoreHelperExtensions
+    {
+        public static HtmlString ResponsiveClasses(this SitecoreHelper sitecoreHelper)
+        {
+            List<string> classes = new List<string>();
+            var renderingParameter = sitecoreHelper.CurrentRendering.Parameters;
+            var fieldNames = GetRenderingResponsiveFieldNames(sitecoreHelper.CurrentRendering);
+
+            foreach(var fieldName in fieldNames)
+            {
+                if(sitecoreHelper.CurrentRendering.Parameters.Contains(fieldName))
+                {
+                    classes.Add(sitecoreHelper.CurrentRendering.Parameters[fieldName]);
+                }
+            }
+
+            return new HtmlString(string.Join(" ", classes.Where(c => !string.IsNullOrWhiteSpace(c))));
+        }
+
+        private static IEnumerable<string> GetRenderingResponsiveFieldNames(Rendering currentRendering)
+        {
+            IEnumerable<string> fields = null;
+            ReferenceField parameterTemplateField = currentRendering.Item.Fields[AppConstants.ParameterTemplateName];
+            if (parameterTemplateField != null && parameterTemplateField.TargetItem != null)
+            {
+                var templateItem = Context.Database.GetTemplate(parameterTemplateField.TargetID);
+                var sections = templateItem.GetSections()
+                    .Where(section => AppConstants.ResponsiveSectionNames
+                    .Contains(section.Name, StringComparer.OrdinalIgnoreCase));
+
+                fields = sections.SelectMany(section => section.GetFields().Select(field => field.Name));
+            }
+
+            return fields;
+        }
+    }
+}

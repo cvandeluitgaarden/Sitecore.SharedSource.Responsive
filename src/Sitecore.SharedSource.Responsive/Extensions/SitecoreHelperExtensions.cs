b06799ba3a1beba0e4a219207121c98b1a0d3f71
Christian van de Luitgaarden <c.vandeluitgaarden@outlook.com>
2015-10-03 13:18:44 +0200

    Modified GetRenderingResponsiveFieldNames method + Added EditorLabel

--- src/Sitecore.SharedSource.Responsive/Extensions/SitecoreHelperExtensions.cs
+++ src/Sitecore.SharedSource.Responsive/Extensions/SitecoreHelperExtensions.cs
@@ -7,6 +7,8 @@
     using System.Linq;
     using System;
     using System.Collections.Generic;
+    using Data.Items;
+    using System.Web.Mvc;
 
     public static class SitecoreHelperExtensions
     {
@@ -27,18 +29,43 @@
             return new HtmlString(string.Join(" ", classes.Where(c => !string.IsNullOrWhiteSpace(c))));
         }
 
+        public static HtmlString EditorLabel(this SitecoreHelper sitecoreHelper, string name)
+        {
+            HtmlString label;
+            if (Sitecore.Context.PageMode.IsPageEditor)
+            {
+                var tag = new TagBuilder("span");
+                tag.AddCssClass("label");
+                tag.SetInnerText(name);
+                label = new HtmlString(tag.ToString());
+            }
+            else
+            {
+                label = new HtmlString(string.Empty);
+            }
+
+            return label;
+        }
+
+        public static HtmlString EditorLabel(this SitecoreHelper sitecoreHelper)
+        {
+            return sitecoreHelper.EditorLabel(sitecoreHelper.CurrentRendering.RenderingItem.DisplayName);
+        }
+
         private static IEnumerable<string> GetRenderingResponsiveFieldNames(Rendering currentRendering)
         {
             IEnumerable<string> fields = new string[] { };
-            ReferenceField parameterTemplateField = currentRendering.Item.Fields[AppConstants.ParameterTemplateName];
+
+            Item renderingItem = Sitecore.Context.Database.GetItem(currentRendering.RenderingItem.ID);
+
+            ReferenceField parameterTemplateField = renderingItem.Fields[AppConstants.ParameterTemplateName];
             if (parameterTemplateField != null && parameterTemplateField.TargetItem != null)
             {
                 var templateItem = Context.Database.GetTemplate(parameterTemplateField.TargetID);
-                var sections = templateItem.GetSections()
-                    .Where(section => AppConstants.ResponsiveSectionNames
-                    .Contains(section.Name, StringComparer.OrdinalIgnoreCase));
 
-                fields = sections.SelectMany(section => section.GetFields().Select(field => field.Name));
+                fields = templateItem.Fields.Where(field => AppConstants.ResponsiveSectionNames
+                    .Contains(field.Section.Name, StringComparer.OrdinalIgnoreCase))
+                    .Select(x => x.Name);
             }
 
             return fields;
